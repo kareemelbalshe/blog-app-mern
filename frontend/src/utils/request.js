59617bb2f1c5c9abcf0c39593998515edd96e12a
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2023-10-30 15:48:08 +0200

    Update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: "http://localhost:5005"
+    baseURL: "https://blog-app-mern-iw0g.onrender.com"
 })
 
 export default request
