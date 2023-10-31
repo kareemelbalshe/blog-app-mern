0cd492034d0578158c2ca74d18a47be575926155
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2023-10-31 19:14:20 +0200

    Update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: "https://blog-app-mern-fprg.onrender.com"
+    baseURL: "https://blog-app-mern-taupe-seven.vercel.app/"
 })
 
 
